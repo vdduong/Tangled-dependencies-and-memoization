1e81151c25fcff8f3fd2924d55523bd7f134eba0
vdduong <vdduong@free.fr>
2014-08-17 18:48:35 +0200

    Create DAG.py

---
+++ DAG.py
@@ -0,0 +1,2 @@
+# overlapping subproblems: finding the distance b-f requires finding the shortest path, for ex, d-f.
+# the magic of memoization removes all the redundancy, and we end up with a linear time algorithm
